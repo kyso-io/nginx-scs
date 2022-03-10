e4463849745ec004d2d9e4424b89f6d08b23d9c7
Sergio Talens-Oliag <sto@kyso.io>
2022-03-10 18:02:51 +0100

    Pass Authorization & X-Original-URI

--- docker-entrypoint.d/10-update-default-conf.sh
+++ docker-entrypoint.d/10-update-default-conf.sh
@@ -19,9 +19,11 @@ server {
   }
   location /.auth {
     internal;
+    proxy_pass $AUTH_REQUEST_URI;
+    proxy_pass_header Authorization;
     proxy_pass_request_body off;
     proxy_set_header Content-Length "";
-    proxy_pass $AUTH_REQUEST_URI;
+    proxy_set_header X-Original-URI \$request_uri;
   }
   error_page   500 502 503 504  /50x.html;
   location = /50x.html {
